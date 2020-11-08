7ed15263bb7471ae29d7c58d70c21a981558aa92
Thuff30 <24842812+Thuff30@users.noreply.github.com>
2020-11-08 17:42:32 -0500

    Update SQLSetup.sql
    Modified properties for Client and Carrier tables. Modified stored procedures and removed stored procedures to view clients and carriers.

--- MySQL/SQLSetup.sql
+++ MySQL/SQLSetup.sql
@@ -2,13 +2,13 @@ CREATE DATABASE Shipping;
 
 CREATE TABLE Client(
 ClientID int AUTO_INCREMENT,
-BusinessName varchar(50) NOT NULL,
+BusinessName varchar(50) NOT NULL UNIQUE,
 CONSTRAINT PRIMARY KEY (ClientID)
 );
 
 CREATE TABLE Carrier(
 CarrierID INT AUTO_INCREMENT,
-CarrierName varchar(20) NOT NULL,
+CarrierName varchar(20) NOT NULL UNIQUE,
 TrackingSite varchar(60),
 CONSTRAINT PRIMARY KEY (CarrierID)
 );
@@ -40,22 +40,12 @@ CONSTRAINT PRIMARY KEY (Username)
 DELIMITER //
 CREATE PROCEDURE FindClient(IN clientName VARCHAR(30))
 BEGIN
-    SELECT CLientID FROM Client WHERE BusinessName=clientName;
+    SELECT ClientID FROM Client WHERE BusinessName=clientName;
 END//
 
 CREATE PROCEDURE FindCarrier(IN carrierin VARCHAR(10))
 BEGIN
     SELECT CarrierID FROM Carrier WHERE CarrierName=carrierin;
-END//
-
-CREATE PROCEDURE showCarriers(OUT Carriers VARCHAR(100))
-BEGIN
-    SELECT CarrierName from Carrier;
-END//
-
-CREATE PROCEDURE showClient(OUT Clients VARCHAR(100))
-BEGIN 
-    SELECT BusinessName from Client;
 END//
 
 CREATE PROCEDURE viewShips(IN shipID INT)
@@ -65,16 +55,15 @@ BEGIN
 				JOIN Carrier ON Carrier.CarrierID=Shipment.CarrierID) WHERE ShipmentID=shipID;
 END//
 
-CREATE PROCEDURE insertShip(IN client INT, IN carrier INT, IN item VARCHAR(100), IN shipped DATE, IN deliver DATE, IN track INT, IN stat VARCHAR(10))
+CREATE PROCEDURE insertShip(IN client INT, IN carrier INT, IN item VARCHAR(100), IN shipped DATE, IN deliver DATE, IN track INT, IN stat VARCHAR(10), IN notes VARCHAR(100), IN entered DATE)
 BEGIN
-    INSERT INTO Shipping.Shipment (ClientID, CarrierID, ItemsShipping, EstShipDate, EstDelivery, TrackingNum, Status, DateEntered)
-				VALUES (client, carrier, item, shipped, deliver, track, stat);
+    INSERT INTO Shipping.Shipment (ClientID, CarrierID, ItemsShipping, EstShipDate, EstDelivery, TrackingNum, Status, Notes, DateEntered)
+				VALUES (client, carrier, item, shipped, deliver, track, stat, notes, entered);
 END//
 
 CREATE PROCEDURE newClient(IN client VARCHAR(50))
 BEGIN
-    INSERT INTO Shipping.Client (BusinessName) VALUES (client) WHERE NOT EXISTS(
-        SELECT BusinessName FROM Shipping.Client WHERE BusinessName=client);
+    INSERT IGNORE INTO Shipping.Client (BusinessName) VALUES (client);
 END//
 
 DELIMITER ;

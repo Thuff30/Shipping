ff017ba4a19241bc95277e3078a1da26dc3ceb86
Thuff30 <24842812+Thuff30@users.noreply.github.com>
2020-11-08 17:57:20 -0500

    Update AddShipForm.php
    Modified form to add estimated delivery date field and modified code formatting for readability.

--- AddShipForm.php
+++ AddShipForm.php
@@ -8,43 +8,43 @@
 	<form name='newship' method='post' action='NewShipment.php'>
 		<table id='newship'>
 			<tr>
-				<th>New Shipment</th>
-				<th></th>
+				<th colspan=2>New Shipment</th>
 			</tr>
 			<tr>
-				<td class="a">Client <input list='client'>
-					<datalist id='client'>";
-					<?php clientDropdown(); ?><!--displays current list of clients from database-->
+				<td class="a">Client <input type="text" list='client' name='client'>
+					<datalist id='client'>
+					<?php clientDropdown(); ?>
 					</datalist>
 				</td>
-				<td class="b">Carrier <input list='carrier'>
-					<datalist id='carrier'>";
-					<?php carrierDropdown();?><!--displays current list of carriers from database-->
+				<td class="b">Carrier <input type='text' list='carrier' name='carrier'>
+					<datalist id='carrier'>
+						<?php carrierDropdown(); ?><!--displays current list of carriers from database-->
 					</datalist>
 				</td>
 			</tr>
 			<tr>
-				<td class="a">Ship Date <input name='shipdate' type='text'></td>
-				<td class="b">Shipment Status <select name='status'>
+				<td class='a'>Ship Date <input name='shipdate' type='text'></td>
+				<td class='b'>Est Delivery Date <input name='deliverydate' type='text'></td>
+			</tr>
+			<tr>
+				<td class='a'>Items Shipped: <input type="textbox" name="items"></td>
+				<td class='b'>Notes: <input type="textbox" name="notes"></td>
+			</tr>
+			<tr>
+				<td class='a'>Tracking Number <input name='tracknum' type='text'></td>
+				<td class='b'>Shipment Status <select name='status'>
 					<option value='InTransit'>In Transit</option>
 					<option value='Delivered'>Delivered</option>
 					<option value='OnHold'>On Hold</option>
 				</select></td>
 			</tr>
-						<tr>
-				<td>Items Shipped: <input type="textbox" name="items"></td>
-				<td>Notes: <input type="textbox" name="notes"></td>
-			</tr>
 			<tr>
-				<td class="a">Tracking Number <input name='tracknum' type='text'></td>
-				<td></td>
-			</tr>
-			<tr>
-				<td id="mybuttona"><input type='submit' value='Search' name='Submit'></td>
+				<td id="mybuttona"><input type='submit' value='Submit' name='Submit'></td>
 				<td id="mybuttonb"><input type='reset' value='Reset Form' name='Reset'></td>
-				
+			</tr>
 		</table>
 	</form>
+	<p></p>
 	<p id="note">NOTE: All fields must be filled</p>
 </body>
 </html>
